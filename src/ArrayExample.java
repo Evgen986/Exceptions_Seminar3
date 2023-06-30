b1d4b67b3e78c8b822f8cbff0b6b0580474c15cc
Evgenyi_Malyutin <evgen986@mail.ru>
2023-06-30 10:31:02 +0300

    initial commit

---
+++ src/ArrayExample.java
@@ -0,0 +1,19 @@
+public class ArrayExample {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        try {
+            int element = getElementAtIndex(array, 6);
+            System.out.println("Значение элемента: " + element);
+            System.out.println(2 + element);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка: Индекс выходит за пределы массива");
+        }
+    }
+
+    public static int getElementAtIndex(int[] array, int index) {
+        return array[index];
+    }
+}
+
---
+++ src/DataProcessor.java
@@ -0,0 +1,21 @@
+//import java.io.IOException;
+//
+//public class DataProcessor {
+//    private FileReader fileReader;
+//
+//    public DataProcessor(FileReader fileReader) {
+//        this.fileReader = fileReader;
+//    }
+//
+//    public void processFile(String filePath) throws IOException {
+//        try {
+//            fileReader.readFile(filePath);
+//            // Обработка данных из файла
+//            // ...
+//        } catch (IOException e) {
+//            // Переброс исключения выше
+//            throw e;
+//        }
+//    }
+//
+//}
